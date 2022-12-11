50c2478fbd5cf3cd9c6eae5660dc4cfe519ae4b4
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-11 19:35:42 +0500

    updated arguments loop

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -46,13 +46,14 @@ public class MethodParser {
             i++;
         }
         List<MethodSignature.Argument> argumentsList = new ArrayList<>();
-        for (int j=0; j<typeAndName.size()/2; j++){
+        for (int j=0; j<typeAndName.size(); j+=2){
             MethodSignature.Argument argument = new MethodSignature.Argument(typeAndName.get(j), typeAndName.get(j+1));
+            System.out.println(argument.getType());
+            System.out.println(argument.getName());
             argumentsList.add(argument);
         }
         methodSignature.setArguments(argumentsList);
 
         return methodSignature;
     }
-
 }
